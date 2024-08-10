1b8d0904cf91c3d5001396bb4f653bf37622370c
cor <cor@pruijs.dev>
2024-08-10 22:26:12 +0200

    chore: init flake

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "Voyager keymap that emulates the Helix editor, built in QMK";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+  };
+
+  outputs = inputs@{ self, flake-parts, nixpkgs }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "aarch64-darwin"
+      ];
+      perSystem = { config, pkgs, ... }: {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [ nixd nixpkgs-fmt cmake-language-server ];
+        };
+      };
+    };
+}
